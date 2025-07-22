5688e158f0c480015a0146432c32b70d0945d4dc
ridhesh w <walavalkarridhesh@gmail.com>
2025-07-22 10:39:38 +0545

    init

---
+++ macos/NearbyFiles/NearbyFiles/NearbyFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyFilesApp.swift
+//  NearbyFiles
+//
+//  Created by ridhesh on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct NearbyFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
